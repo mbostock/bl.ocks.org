4b2cb944e37f963f65fcfc0d89bc3ab428627cce
Mike Bostock <mbostock@gmail.com>
2014-03-01 10:49:00 -0800

    Fixes.

--- chrome/bl.ocks.chrome/blocks.js
+++ chrome/bl.ocks.chrome/blocks.js
@@ -15,13 +15,13 @@ function redraw() {
   var a = document.querySelector("#bl-ocks-org");
   if (!a) {
     var li = document.createElement("li");
-    li.innerHTML = '<a class="minibutton" id="bl-ocks-org"><span class="mini-icon mini-icon-external-link"></span>bl.ocks.org</a>';
+    li.innerHTML = '<a class="minibutton" id="bl-ocks-org"><span class="octicon octicon-link-external" style="width:16px;text-align:center;"></span>bl.ocks.org</a>';
     a = li.firstChild;
     ul.insertBefore(li, ul.firstChild);
   }
 
   var parts = location.pathname.substring(1).split("/"), user = parts[0], id = parts[1], sha = parts[2];
-  if (!user || !/^[a-z0-9][a-z0-9]*$/.test(user)) return;
+  if (!user || !/^[a-z0-9][a-z0-9]*$/i.test(user)) return;
   if (!/^([0-9]+|[0-9a-f]{20})$/.test(id)) id = null;
   if (!/^[0-9a-f]{40}$/.test(sha)) sha = null;
   a.href = "http://bl.ocks.org/" + user + (id ? "/" + id + (sha ? "/" + sha : "") : "");
