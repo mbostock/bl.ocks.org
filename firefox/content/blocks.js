7dec3b8e64998b355449353495fdf27ad04cb8da
Mike Bostock <mbostock@gmail.com>
2015-05-18 14:57:24 -0700

    Get Firefox working again.

--- firefox/content/blocks.js
+++ firefox/content/blocks.js
@@ -3,34 +3,34 @@ window.addEventListener("load", function load() {
   gBrowser.addEventListener("DOMContentLoaded", function(e) {
     var document = e.originalTarget;
     if (document.location.hostname !== "gist.github.com") return;
+    var anchor,
+        observer = new MutationObserver(redraw); // TODO debounce
+
+    observer.observe(document.documentElement, {childList: true, subtree: true});
 
     redraw();
-    document.addEventListener("DOMSubtreeModified", redraw);
 
     function redraw() {
-      var root = document.querySelector(".root-pane");
-      if (!root) return;
-
-      var ul = document.querySelector(".export-references");
-      if (!ul) {
-        ul = document.createElement("ul");
-        ul.className = "export-references";
-        root.appendChild(ul);
-      }
-
-      var a = document.querySelector("#bl-ocks-org");
-      if (!a) {
-        var li = document.createElement("li");
-        li.innerHTML = '<a class="minibutton" id="bl-ocks-org"><span class="octicon octicon-link-external" style="width:16px;text-align:center;"></span>bl.ocks.org</a>';
-        a = li.firstChild;
-        ul.insertBefore(li, ul.firstChild);
-      }
+      var container = document.querySelector(".only-with-full-nav");
+      if (!container) return;
 
       var parts = document.location.pathname.substring(1).split("/"), user = parts[0], id = parts[1], sha = parts[2];
       if (!user || !/^[a-z0-9][a-z0-9]*$/i.test(user)) return;
       if (!/^([0-9]+|[0-9a-f]{20})$/.test(id)) id = null;
       if (!/^[0-9a-f]{40}$/.test(sha)) sha = null;
-      a.href = "http://bl.ocks.org/" + user + (id ? "/" + id + (sha ? "/" + sha : "") : "");
+
+      if (!anchor) {
+        anchor = document.createElement("a");
+        anchor.className = "minibutton sidebar-button";
+        anchor.innerHTML = '<span class="octicon octicon-link-external"></span>bl.ocks.org';
+      }
+
+      anchor.href = "http://bl.ocks.org/" + user + (id ? "/" + id + (sha ? "/" + sha : "") : "");
+
+      // Disconnect to avoid infinite loop in Firefox. :\
+      observer.disconnect();
+      container.appendChild(anchor);
+      observer.observe(document.documentElement, {childList: true, subtree: true});
     }
   }, false);
 }, false);
