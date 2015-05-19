5616d2f4caf4bdbf99b4dbc52d6b3a46b6a427a6
Mike Bostock <mbostock@gmail.com>
2015-05-19 16:21:54 -0700

    Avoid infinite loop when updating.

--- firefox/content/blocks.js
+++ firefox/content/blocks.js
@@ -3,34 +3,40 @@ window.addEventListener("load", function load() {
   gBrowser.addEventListener("DOMContentLoaded", function(e) {
     var document = e.originalTarget;
     if (document.location.hostname !== "gist.github.com") return;
-    var anchor,
-        observer = new MutationObserver(redraw); // TODO debounce
+    var observer = new MutationObserver(redraw);
 
     observer.observe(document.documentElement, {childList: true, subtree: true});
 
     redraw();
 
     function redraw() {
-      var container = document.querySelector(".only-with-full-nav");
+      var container = document.querySelector(".gist-sidebar");
       if (!container) return;
 
-      var parts = document.location.pathname.substring(1).split("/"), user = parts[0], id = parts[1], sha = parts[2];
+      var parts = document.location.pathname.substring(1).split("/"),
+          user = parts[0],
+          id = parts[1],
+          sha = parts[2];
       if (!user || !/^[a-z0-9][a-z0-9]*$/i.test(user)) return;
       if (!/^([0-9]+|[0-9a-f]{20})$/.test(id)) id = null;
       if (!/^[0-9a-f]{40}$/.test(sha)) sha = null;
 
+      var anchor = container.querySelector(".bl-ocks-button"),
+          href = "http://bl.ocks.org/" + user + (id ? "/" + id + (sha ? "/" + sha : "") : "");
+
       if (!anchor) {
         anchor = document.createElement("a");
-        anchor.className = "minibutton sidebar-button";
+        anchor.className = "minibutton sidebar-button bl-ocks-button";
         anchor.innerHTML = '<span class="octicon octicon-link-external"></span>bl.ocks.org';
       }
 
-      anchor.href = "http://bl.ocks.org/" + user + (id ? "/" + id + (sha ? "/" + sha : "") : "");
-
-      // Disconnect to avoid infinite loop in Firefox. :\
-      observer.disconnect();
-      container.appendChild(anchor);
-      observer.observe(document.documentElement, {childList: true, subtree: true});
+      // Disconnect to avoid observing our own mutations.
+      if (anchor.href !== href || anchor.parentNode !== container) {
+        observer.disconnect();
+        anchor.href = href;
+        container.appendChild(anchor);
+        observer.observe(document.documentElement, {childList: true, subtree: true});
+      }
     }
   }, false);
 }, false);
